7c5950ee95d23a73fdc94271a772f82317526b1e
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2019-11-25 10:36:46 -0800

    solving heroku problems

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
 var express = require("express");
-var heroku = require("heroku");
+var heroku = require("heroku"); // need?
 var path = require("path");
 
 // Sets up the Express App
